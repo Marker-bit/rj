510b943ed82a808b948d5d56ca71a7719770c54f
Marker-bit <maarrrkkkk@ya.ru>
2024-02-17 21:59:12 +0300

    anything

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -4,18 +4,19 @@ import { validateRequest } from "@/lib/server-validate-request";
 
 export async function GET(req: NextRequest) {
   const { user } = await validateRequest();
-  if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  if (!user)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   const books = await db.book.findMany({
     where: {
-      userId: user.id
+      userId: user.id,
     },
     include: {
       collections: true,
       readEvents: true,
-      user: false
+      user: false,
     },
   });
-  if (!books) return NextResponse.json([]);
+  console.log(books);
   const compareBooks = (a: any, b: any) => {
     const aPages = a.readEvents[a.readEvents.length - 1]?.pagesRead;
     const bPages = b.readEvents[b.readEvents.length - 1]?.pagesRead;
@@ -26,20 +27,21 @@ export async function GET(req: NextRequest) {
     if (aPages == bPages) return 0;
     if (aPages < bPages) return 1;
     return 0;
-  }
+  };
   const sortedBooks = books.toSorted(compareBooks);
   return NextResponse.json(sortedBooks);
 }
 
 export async function POST(req: NextRequest) {
   const { user } = await validateRequest();
-  if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  if (!user)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   const data = await req.json();
   const book = await db.book.create({
     data: {
       ...data,
-      userId: user.id
-    }
+      userId: user.id,
+    },
   });
   return NextResponse.json(book);
 }
