a393df6c5acf381e922a6ea5bf68ad7d9896c806
Marker-bit <maarrrkkkk@ya.ru>
2024-02-18 08:38:03 +0300

    fix books?

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -16,7 +16,7 @@ export async function GET(req: NextRequest) {
       user: false,
     },
   });
-  console.log(books, typeof books);
+  console.log(books, Array.isArray(books));
   const compareBooks = (a: any, b: any) => {
     const aPages = a.readEvents[a.readEvents.length - 1]?.pagesRead;
     const bPages = b.readEvents[b.readEvents.length - 1]?.pagesRead;
