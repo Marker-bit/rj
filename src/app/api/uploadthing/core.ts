f66bfcde3d4da8aec3ab6d6a2dd7c416e2571973
Marker-bit <maarrrkkkk@ya.ru>
2024-02-20 12:08:37 +0300

    add uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,32 @@
+import { validateRequest } from "@/lib/server-validate-request";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+ 
+const f = createUploadthing();
+ 
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  bookCover: f({ image: { maxFileSize: "8MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const {user} = await validateRequest();
+ 
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError("Unauthorized");
+ 
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+ 
+      console.log("file url", file.url);
+ 
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId, url: file.url };
+    }),
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
