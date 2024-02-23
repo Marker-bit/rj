28f72f27fbf693e1a5b36d97407504776b765193
Marker-bit <maarrrkkkk@ya.ru>
2024-02-23 10:53:24 +0300

    fix bugs, add collections

---
+++ src/app/collections/create-collection.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { DialogHeader } from "@/components/ui/dialog";
+import { DrawerDialog } from "../Drawer";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Поле обязательно для заполнения" }),
+});
+
+export function CreateCollection() {
+  const [open, setOpen] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  function handleClose(b: boolean) {
+    setOpen(b);
+    if (!b) {
+      form.reset();
+    }
+  }
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)} className="md:w-fit items-center gap-2 m-2">
+        <Plus className="w-4 h-4" />
+        Создать коллекцию
+      </Button>
+      <DrawerDialog open={open} onOpenChange={handleClose}>
+        <DialogHeader>
+          <DialogTitle>Создать коллекцию</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="md:min-w-[40vw]">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Название</FormLabel>
+                  <FormControl>
+                    <Input {...field} autoFocus />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="mt-2 ml-auto">Создать</Button>
+          </form>
+        </Form>
+      </DrawerDialog>
+    </>
+  );
+}
