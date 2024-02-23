434da75099069077d4fbb3c1b9bcd07c5371b3e8
Marker-bit <maarrrkkkk@ya.ru>
2024-02-23 20:01:37 +0300

    change back

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // Add a rule for node-loader to handle the binary files
-    config.module.rules.push({
-      test: /\.node$/,
-      use: 'node-loader',
-    });
-
-    // Your existing webpack configuration
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
   },
