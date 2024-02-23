da28a02e736bc133e1b836a7e29c9ba714727185
Marker-bit <maarrrkkkk@ya.ru>
2024-02-23 19:41:36 +0300

    move to a new file

--- src/app/BookView.tsx
+++ src/app/BookView.tsx
@@ -46,6 +46,7 @@ import { dateToString } from "@/lib/utils";
 import { isSameDay } from "date-fns";
 import { UploadButton } from "@/components/uploadthing";
 import { EditBookModal } from "@/components/dialogs/edit-book-modal";
+import { BookInfoModal } from "@/components/dialogs/book-info-modal";
 
 const bookSchema = z.object({
   title: z.string().min(1),
@@ -63,17 +64,6 @@ export function BookView({ book }: { book: Book }) {
   const [actionsDrawerOpen, setActionsDrawerOpen] = useState(false);
   const [descriptionDrawerOpen, setDescriptionDrawerOpen] = useState(false);
   const isMobile = useMediaQuery("(max-width: 768px)");
-
-  const form = useForm<z.infer<typeof bookSchema>>({
-    resolver: zodResolver(bookSchema),
-    defaultValues: {
-      title: book.title,
-      author: book.author,
-      pages: book.pages,
-      description: book.description ?? "",
-      coverUrl: book.coverUrl ?? "",
-    },
-  });
 
   const undoEventMutation = useMutation({
     mutationFn: () =>
@@ -134,22 +124,6 @@ export function BookView({ book }: { book: Book }) {
     },
   });
 
-  const editMutation = useMutation({
-    mutationFn: (values: z.infer<typeof bookSchema>) =>
-      fetch(`/api/books/${book.id}/`, {
-        method: "PATCH",
-        body: JSON.stringify(values),
-      }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["books"],
-      });
-      queryClient.invalidateQueries({
-        queryKey: ["events"],
-      });
-    },
-  });
-
   const deleteMutation = useMutation({
     mutationFn: () =>
       fetch(`/api/books/${book.id}/`, {
@@ -170,11 +144,6 @@ export function BookView({ book }: { book: Book }) {
 
   const lastEvent = book.readEvents[0];
 
-  async function onSubmit(values: z.infer<typeof bookSchema>) {
-    await editMutation.mutateAsync(values);
-    setEditOpen(false);
-  }
-
   return (
     <div
       className="border border-zinc-200 p-2 rounded-md hover:shadow transition-shadow flex gap-2 group relative"
@@ -191,115 +160,16 @@ export function BookView({ book }: { book: Book }) {
           {book.description}
         </pre>
       </DrawerDialog>
-      <DrawerDialog
+      <BookInfoModal
         open={actionsDrawerOpen}
-        onOpenChange={setActionsDrawerOpen}
-      >
-        <div className="flex gap-2 flex-col mt-2">
-          <div className="flex">
-            {book.coverUrl && (
-              <Image
-                src={book.coverUrl}
-                alt="book"
-                width={500}
-                height={500}
-                className="rounded-md h-52 w-auto"
-              />
-            )}
-            <div className="flex flex-col m-2 mt-0">
-              <div className="font-bold text-xl">{book.title}</div>
-              <div className="text-sm">{book.author}</div>
-              <div className="text-sm">{book.pages} страниц</div>
-              <div className="w-fit">
-                {lastEvent?.pagesRead === book.pages && (
-                  <Badge>Прочитана</Badge>
-                )}
-                {!lastEvent && <Badge>Запланирована</Badge>}
-                {lastEvent && lastEvent?.pagesRead !== book.pages && (
-                  <Badge>Читается</Badge>
-                )}
-              </div>
-              {book.description && (
-                <pre
-                  className="relative text-black/70 overflow-hidden font-sans block mt-2 cursor-pointer text-wrap"
-                  onClick={() => setDescriptionDrawerOpen(true)}
-                >
-                  {book.description.split("\n").slice(0, 5).join("\n")}
-                  {book.description.split("\n").length > 5 && "..."}
-                </pre>
-              )}
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            {book.readEvents.map((event) => (
-              <div className="flex items-center gap-2" key={event.id}>
-                {event.pagesRead === book.pages ? (
-                  <>
-                    <BookOpenCheck className="text-green-500 w-4 h-4" />
-                    <div className="flex flex-col">
-                      <div>Книга прочитана!</div>
-                      <div className="text-xs text-black/50">
-                        {dateToString(new Date(event.readAt))}
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <BookOpen className="w-4 h-4" />
-                    <div className="flex flex-col">
-                      <div>{event.pagesRead} страниц прочитано</div>
-                      <div className="text-xs text-black/50">
-                        {dateToString(new Date(event.readAt))}
-                      </div>
-                    </div>
-                  </>
-                )}
-              </div>
-            ))}
-          </div>
-          <Button
-            className="gap-2"
-            variant="outline"
-            onClick={() => setEditOpen(true)}
-          >
-            <Edit className="w-4 h-4" />
-            Редактировать
-          </Button>
-          <Button
-            className="gap-2 text-red-500 hover:text-red-700"
-            variant="outline"
-            onClick={() => setDeleteDialogOpen(true)}
-          >
-            <Trash className="w-4 h-4" />
-            Удалить
-          </Button>
-          {lastEvent?.pagesRead !== book.pages && (
-            <>
-              <Button
-                className="gap-2"
-                variant="outline"
-                disabled={doneMutation.isPending}
-                onClick={() => doneMutation.mutate()}
-              >
-                {doneMutation.isPending ? (
-                  <Loader className="w-4 h-4 animate-spin" />
-                ) : (
-                  <BookOpenCheck className="w-4 h-4" />
-                )}
-                Прочитана
-              </Button>
-              <Button
-                className="gap-2"
-                variant="outline"
-                onClick={() => setDateOpen(true)}
-              >
-                <BookOpenTextIcon className="w-4 h-4" />
-                Отметить прочтение
-              </Button>
-            </>
-          )}
-        </div>
-      </DrawerDialog>
+        setOpen={setActionsDrawerOpen}
+        book={book}
+        setDescriptionDrawerOpen={setDescriptionDrawerOpen}
+        setEditOpen={setEditOpen}
+        setDeleteDialogOpen={setDeleteDialogOpen}
+        setDateOpen={setDateOpen}
+        doneMutation={doneMutation}
+      />
       <DateReadModal
         isOpen={dateOpen}
         setIsOpen={setDateOpen}
@@ -380,20 +250,6 @@ export function BookView({ book }: { book: Book }) {
                   <Undo className="w-4 h-4" />
                 )}
               </Button>
-              {/* <div className="bg-green-100 flex gap-2 items-center text-green-500 px-3 rounded-xl cursor-default">
-                <BookOpen className="w-4 h-4" /> {lastEvent.pagesRead} страниц
-                из {book.pages}
-                <button
-                  className="p-1 hover:bg-green-500/10 transition-all cursor-pointer rounded-md my-1"
-                  onClick={() => undoEventMutation.mutate()}
-                >
-                  {undoEventMutation.isPending ? (
-                    <Loader className="w-4 h-4 animate-spin" />
-                  ) : (
-                    <Undo className="w-4 h-4" />
-                  )}
-                </button>
-              </div> */}
             </>
           )}
         </div>
