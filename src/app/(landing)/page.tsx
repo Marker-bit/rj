7179fd7e5ff2244fbb9d1e294dd5875119fa3a3f
Marker-bit <maarrrkkkk@ya.ru>
2024-03-02 13:01:32 +0300

    update main

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -29,6 +29,7 @@ export default async function LandingPage({
               height={100}
               alt="logo"
               className="size-14"
+              loading="eager"
             />
           </div>
           <h1 className="text-5xl md:text-7xl font-black text-center">
