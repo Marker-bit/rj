1adcb3711ca141caf524c0a975c07a457e0e7cfe
Marker-bit <maarrrkkkk@ya.ru>
2024-03-02 11:28:40 +0300

    add landing, fix books sort

--- src/app/(app)/api/books/route.ts
+++ src/app/(app)/api/books/route.ts
@@ -1,37 +1,13 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { validateRequest } from "@/lib/server-validate-request";
+import { fetchBooks } from "@/lib/books";
 
 export async function GET(req: NextRequest) {
   const { user } = await validateRequest();
   if (!user)
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  const books = await db.book.findMany({
-    where: {
-      userId: user.id,
-    },
-    include: {
-      collections: true,
-      readEvents: {
-        orderBy: {
-          readAt: "desc"
-        }
-      },
-      user: false,
-    },
-  });
-  const compareBooks = (a: any, b: any) => {
-    const aPages = a.readEvents[a.readEvents.length - 1]?.pagesRead;
-    const bPages = b.readEvents[b.readEvents.length - 1]?.pagesRead;
-    if (!aPages && !bPages) return 0;
-    if (!aPages) return 1;
-    if (!bPages) return -1;
-    if (aPages > bPages) return -1;
-    if (aPages == bPages) return 0;
-    if (aPages < bPages) return 1;
-    return 0;
-  };
-  books.sort(compareBooks);
+  const books = await fetchBooks(user.id);
   return NextResponse.json(books);
 }
 
