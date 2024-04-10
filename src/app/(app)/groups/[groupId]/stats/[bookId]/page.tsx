f67b694aa510305afb4af85ae4f1398b8dd253dd
Marker-bit <maarrrkkkk@ya.ru>
2024-04-10 15:50:38 +0300

    fix another bug

--- src/app/(app)/groups/[groupId]/stats/[bookId]/page.tsx
+++ src/app/(app)/groups/[groupId]/stats/[bookId]/page.tsx
@@ -76,9 +76,9 @@ export default async function Page({
           ?.pagesRead || 0)
   );
 
-  const ratingKeys = Object.keys(rating)?.toSorted(
-    (a, b) => rating[b] - rating[a]
-  ) || Object.keys(rating);
+  const ratingKeys = new Array(...Object.keys(rating));
+
+  ratingKeys.sort((a, b) => rating[b] - rating[a]);
 
   return (
     <div className="p-8 flex flex-col">
