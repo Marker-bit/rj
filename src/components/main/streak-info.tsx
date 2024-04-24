fbd04f6a40e896eea760aa335e8759e9c22882aa
Marker-bit <maarrrkkkk@ya.ru>
2024-04-24 21:59:29 +0300

    update streak info

--- src/components/main/streak-info.tsx
+++ src/components/main/streak-info.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { validateRequest } from "@/lib/server-validate-request";
 import { getStreak } from "@/lib/stats";
 import { PartyPopper } from "lucide-react";
+import { StreakCounter } from "./streak-counter";
 
 export async function StreakInfo() {
   const { user } = await validateRequest();
@@ -21,25 +22,7 @@ export async function StreakInfo() {
     },
   });
 
-  const streak = getStreak(events);
-
-  if ((streak % 50 !== 0 && user.id !== "clsqfrmec000013rgpmmb8eok") || streak === 0) {
-    return <></>;
-  }
-
   return (
-    <div className="flex gap-2 bg-orange-500 text-white p-2 rounded-xl m-2 overflow-hidden">
-      <div className="text-9xl font-bold">{streak}</div>
-      <div className="max-sm:hidden">
-        <div className="text-2xl font-bold">Ваша активность</div>
-        <div className="text-lg">Сколько дней вы читаете подряд</div>
-      </div>
-      <div className="hidden max-sm:block">
-        <div className="text-2xl font-bold">Чтение подряд</div>
-      </div>
-      <div className="ml-auto mt-auto translate-x-8 translate-y-8">
-        <PartyPopper className="w-32 h-32 text-orange-200 -scale-x-100" />
-      </div>
-    </div>
+    <StreakCounter events={events} user={user} />
   );
 }
