b1482ee94b1a5b8bdfdf7e3d72e842a139730436
Marker-bit <maarrrkkkk@ya.ru>
2024-06-13 19:01:01 +0300

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const font = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   metadataBase: new URL("https://rj-ten.vercel.app/"),
   title: "Читательский дневник",
-  description: "Читательский дневник",
+  description: "Читательский дневник - социальная сеть для читателей",
   manifest: "https://rj-ten.vercel.app/manifest.json",
   twitter: {
     card: "summary_large_image",
@@ -38,126 +38,126 @@ export const metadata: Metadata = {
   icons: {
     apple: [
       {
-        url: "https://rj-ten.vercel.app/icons/icon-72x72.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-96x96.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-128x128.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-144x144.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-152x152.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-192x192.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-384x384.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-512x512.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
     ],
     other: [
       {
-        url: "https://rj-ten.vercel.app/icons/icon-72x72.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-96x96.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-128x128.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-144x144.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-152x152.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-192x192.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-384x384.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-512x512.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
     ],
     icon: [
       {
-        url: "https://rj-ten.vercel.app/icons/icon-72x72.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-96x96.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-128x128.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-144x144.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-152x152.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-192x192.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-384x384.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/icons/icon-512x512.png",
+        url: "https://rj-ten.vercel.app/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
