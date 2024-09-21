402171b086fc4270db71545cfed8ff90070375ba
Marker-bit <maarrrkkkk@ya.ru>
2024-09-21 15:12:35 +0300

    added landing button

--- src/app/(landing)/landing.tsx
+++ src/app/(landing)/landing.tsx
@@ -2,13 +2,25 @@
 import React from "react"
 import { ContainerScroll } from "@/components/effects/container-scroll-animation"
 import Image from "next/image"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { ChevronRight } from "lucide-react";
 
 export function Landing({ loggedIn }: { loggedIn: boolean }) {
   return (
     <div className="flex flex-col items-center overflow-hidden py-10">
-      <h1 className="mt-10 text-4xl font-bold text-black dark:text-white md:text-[6rem]">
-        Читательский дневник
-      </h1>
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="my-10 text-4xl font-bold text-black dark:text-white md:text-[6rem]">
+          Читательский дневник
+        </h1>
+
+        <Button size="lg" asChild>
+          <Link href={loggedIn ? "/home" : "/auth/register"} className="flex items-center gap-2">
+            {loggedIn ? "Войти" : "Начать"}
+            <ChevronRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
       <ContainerScroll titleComponent={<></>}>
         <Image
           src={`/rjrj.png`}
