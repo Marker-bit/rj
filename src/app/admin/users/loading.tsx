2655f4ccef70e7a3d165749d990e1af99fc2f691
Marker-bit <maarrrkkkk@ya.ru>
2024-09-21 21:46:55 +0300

    test commit

--- src/app/admin/users/loading.tsx
+++ src/app/admin/users/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="m-2">
+    <div className="m-2 ">
       <Skeleton className="h-10 w-48 rounded-md" />
       <div className="mt-2 flex flex-col gap-2">
         <Skeleton className="h-16 w-full rounded-xl" />
