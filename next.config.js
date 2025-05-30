270c07e59e83e6de71c6f90517039d5668d3b437
Marker-bit <maarrrkkkk@ya.ru>
2025-05-30 17:24:45 +0300

    fix old images

--- next.config.js
+++ next.config.js
@@ -32,6 +32,11 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "*.labirint.ru",
         pathname: "/**",
       },
