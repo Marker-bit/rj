c37d7e3758d45d9fa7ae8a2d95689d49f383ddf0
Marker-bit <maarrrkkkk@ya.ru>
2025-06-03 10:38:41 +0300

    update pwas and some legacy code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,25 +16,25 @@ import { ourFileRouter } from "@/app/(app)/api/uploadthing/core"
 const font = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://rj-ten.vercel.app/"),
+  metadataBase: new URL("https://rjrj.ru/"),
   title: "Читательский дневник",
   description: "Читательский дневник - социальная сеть для читателей",
-  manifest: "https://rj-ten.vercel.app/manifest.json",
+  manifest: "https://rjrj.ru/manifest.json",
   twitter: {
     card: "summary_large_image",
     title: "Reading Journal",
     description: "A service for reading fans",
-    images: ["https://rj-ten.vercel.app/og.png"],
+    images: ["https://rjrj.ru/og.png"],
   },
   openGraph: {
     title: "Читательский дневник",
     description: "Читательский дневник",
-    url: "https://rj-ten.vercel.app/",
-    images: ["https://rj-ten.vercel.app/og.png"],
+    url: "https://rjrj.ru/",
+    images: ["https://rjrj.ru/og.png"],
   },
   appLinks: {
     web: {
-      url: "https://rj-ten.vercel.app/",
+      url: "https://rjrj.ru/",
     },
   },
   creator: "Mark Pentus",
@@ -42,133 +42,130 @@ export const metadata: Metadata = {
   icons: {
     apple: [
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-72x72.png",
+        url: "https://rjrj.ru/images/icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-96x96.png",
+        url: "https://rjrj.ru/images/icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-128x128.png",
+        url: "https://rjrj.ru/images/icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-144x144.png",
+        url: "https://rjrj.ru/images/icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-152x152.png",
+        url: "https://rjrj.ru/images/icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-192x192.png",
+        url: "https://rjrj.ru/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-384x384.png",
+        url: "https://rjrj.ru/images/icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-512x512.png",
+        url: "https://rjrj.ru/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
     ],
     other: [
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-72x72.png",
+        url: "https://rjrj.ru/images/icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-96x96.png",
+        url: "https://rjrj.ru/images/icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-128x128.png",
+        url: "https://rjrj.ru/images/icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-144x144.png",
+        url: "https://rjrj.ru/images/icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-152x152.png",
+        url: "https://rjrj.ru/images/icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-192x192.png",
+        url: "https://rjrj.ru/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-384x384.png",
+        url: "https://rjrj.ru/images/icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-512x512.png",
+        url: "https://rjrj.ru/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
     ],
     icon: [
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-72x72.png",
+        url: "https://rjrj.ru/images/icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-96x96.png",
+        url: "https://rjrj.ru/images/icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-128x128.png",
+        url: "https://rjrj.ru/images/icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-144x144.png",
+        url: "https://rjrj.ru/images/icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-152x152.png",
+        url: "https://rjrj.ru/images/icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-192x192.png",
+        url: "https://rjrj.ru/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-384x384.png",
+        url: "https://rjrj.ru/images/icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        url: "https://rj-ten.vercel.app/images/icons/icon-512x512.png",
+        url: "https://rjrj.ru/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
     ],
-  },
-  other: {
-    ["yandex-verification"]: process.env.YANDEX_VERIFICATION || "",
   },
 }
 
