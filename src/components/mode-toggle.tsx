0689751c4bdf95927acef4b5c2e7a44c5bb25cd8
Marker-bit <maarrrkkkk@ya.ru>
2025-06-05 18:28:00 +0300

    mode toggle icon size

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -19,8 +19,8 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" className="relative rounded-full">
-          <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="size-4 dark:size-0" />
+          <Moon className="absolute size-0 dark:size-4" />
           <span className="sr-only">Переключить тему</span>
         </Button>
       </DropdownMenuTrigger>
