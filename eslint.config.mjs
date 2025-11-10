b7116094478dc5ae9b0b8bedb8c8ab490b2dd6f1
Marker-bit <maarrrkkkk@ya.ru>
2025-11-10 16:18:14 +0300

    improved eslint config and updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,64 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 import next from "eslint-config-next";
+import eslintParserTypeScript from "@typescript-eslint/parser";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
 
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...next, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}]
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  ...next,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    files: ["**/*.{ts,tsx,cts,mts}"],
+    languageOptions: {
+      parser: eslintParserTypeScript,
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  {
+    files: ["**/*.{jsx,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "better-tailwindcss": eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      // enable all recommended rules to report a warning
+      ...eslintPluginBetterTailwindcss.configs["recommended-warn"].rules,
+      // enable all recommended rules to report an error
+      ...eslintPluginBetterTailwindcss.configs["recommended-error"].rules,
 
-export default eslintConfig
+      // or configure rules individually
+      "better-tailwindcss/enforce-consistent-line-wrapping": [
+        "warn",
+        { printWidth: 100 },
+      ],
+    },
+    settings: {
+      "better-tailwindcss": {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        entryPoint: "src/global.css",
+        // tailwindcss 3: the path to the tailwind config file (eg: `tailwind.config.js`)
+        tailwindConfig: "tailwind.config.js",
+      },
+    },
+  },
+];
+
+export default eslintConfig;
